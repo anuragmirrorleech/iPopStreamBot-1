f09c4bdd98b9742ed52950801b54c85cb7aafbba
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2021-12-23 12:29:25 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,29 +1,5 @@
-#(©)Codexbotz
-#(©)Cold_Onez
-
 from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-@Bot.on_callback_query()
-async def cb_handler(client: Bot, query: CallbackQuery):
-    data = query.data
-    if data == "about":
-        await query.message.edit_text(
-            text = f"<b>○ Mᴀsᴛᴇʀ : <a href='https://t.me/COLD_ONEZ'>Cᴏʟᴅ々Oɴᴇᴢ࿐</a>\n○ Lᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ Lɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://github.com/COLD-ONEZ/File-SHare-Bot'>Click Here</a>\n○ Cʜᴀɴɴᴇʟ : @Mo_Tech_YT\n○ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ : @Mo_Tech_Group</b>",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("🔒 Cʟᴏsᴇ", callback_data = "close")
-                    ]
-                ]
-            )
-        )
-    elif data == "close":
-        await query.message.delete()
-        try:
-            await query.message.reply_to_message.delete()
-        except:
-            pass
