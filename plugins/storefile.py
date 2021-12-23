4f206927650ba115a1d47fb144f7878252827a4b
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2021-12-23 21:45:50 +0530

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -39,7 +39,6 @@ async def storefile(c, m):
         await msg.reply(text)
 
     # creating urls
-    string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Sʜᴀʀᴇ URL 🔁", url=f'https://telegram.me/share/url?url={link}')]])
